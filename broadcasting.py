5c4bdf655032b046aba168048a24ce73dd40dab3
njanirudh <anijaya9@gmail.com>
2018-09-14 14:50:01 +0200

    added basics of broadcasting

---
+++ broadcasting.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Broadcasting is the name given to the method
+# that NumPy uses to allow array arithmetic between
+# arrays with a different shape or size.
+
+a = np.array([1, 2, 3])
+print(a)
+
+b = 2
+print(b)
+
+c = a + b
+print(c)
