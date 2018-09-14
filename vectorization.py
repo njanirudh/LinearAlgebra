54ad55107089818076d76736138d2c8428d8381e
njanirudh <anijaya9@gmail.com>
2018-09-14 14:48:52 +0200

    Minor changes to vectorization

--- vectorization.py
+++ vectorization.py
@@ -19,7 +19,18 @@ print(matrix_basic_1 / matrix_basic_2)
 
 # Accessing and slicing array
 # Similar to the python list slicing
-arr = np.arange(0,11)
+arr = np.arange(1,11)
 print(arr[-5:])
 print(arr[::-1])
 
+# Creating shallow and deep copy
+# Shallow copy
+shallow_arr = arr
+
+# Deep copy
+deep_arr = arr.copy()
+
+# Changing shape of array
+# Giving meaning shape
+reshaped_array = arr.reshape(2,5)
+print(reshaped_array)
